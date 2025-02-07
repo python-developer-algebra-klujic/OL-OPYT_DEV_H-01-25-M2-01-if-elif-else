5f86d16f1d3ac5d2dfa0075c17ce8ca3461804bd
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-07 20:11:06 +0100

    Update main_02.py

--- main_02.py
+++ main_02.py
@@ -2,3 +2,17 @@
 Napišite program koji provjerava pripada li unesena riječ vrsti riječi palindrom.
 Palindrom je riječ koja se jednako piše (i čita) s lijeva na desno i s desna na lijevo
 '''
+
+word = input('Upisite rijec koju zelite provjeriti: ')
+word_reversed = '' # a; a + r; ar + b; ...
+
+word_length = len(word)
+
+for i in range(word_length - 1, -1, -1):
+    word_reversed = word_reversed + word[i]
+
+# if word.lower() == word_reversed.lower():
+if word.upper() == word_reversed.upper(): # Srđan Golubović
+    print(f'Rijec {word} je palindrom!')
+else:
+    print(f'Rijec {word} NIJE palindrom!')
