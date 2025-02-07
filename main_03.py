5a17640b4ebe952de5e041df918e9b675a565cc2
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-07 20:33:54 +0100

    Create main_03.py

---
+++ main_03.py
@@ -0,0 +1,16 @@
+text = ('Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+'Ut viverra mauris ac quam viverra congue. '
+'Pellentesque vitae mauris augue. Proin ut mattis neque. '
+'Phasellus ac aliquam metus. Mauris varius posuere dui. '
+'Curabitur sollicitudin nunc elit, eget lacinia velit feugiat in. '
+'Nunc eget est ut purus faucibus maximus eu hendrerit nulla. '
+'Vestibulum nibh urna, condimentum sit amet est at, mollis feugiat lectus.'
+'Duis at est urna. Etiam consectetur eros ut nisi volutpat tempus. '
+'Nulla et purus ac justo dictum consectetur eget in erat. '
+'Donec ut ultricies eros. Curabitur sit amet consectetur diam. '
+'Ut leo turpis, ultrices vitae lacinia vitae, vestibulum at ligula. '
+'Nullam posuere augue vitae nisi dapibus pellentesque. '
+'Praesent ut justo eu erat luctus facilisis eu at enim. '
+'Vivamus auctor sapien in tellus dapibus convallis.')
+
+print(text)
